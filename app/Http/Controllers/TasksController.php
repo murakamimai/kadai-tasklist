92f313b5f9513fc86c0a8a8b02d84756c2a0b3ff
mituko <mituko.misawa@yahoo.co.jp>
2021-07-04 12:47:28 +0000

    Lesson15課題再提出

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -20,7 +20,6 @@ class TasksController extends Controller
             // 認証済みユーザを取得
             $user = \Auth::user();
             // ユーザの投稿の一覧を作成日時の降順で取得
-            // （後のChapterで他ユーザの投稿も取得するように変更しますが、現時点ではこのユーザの投稿のみ取得します）
             $tasks = $user->tasks()->orderBy('created_at', 'desc')->paginate(10);
 
             $data = [
@@ -126,13 +125,16 @@ class TasksController extends Controller
             'status' => 'required|max:10',   // 追加
             'content' => 'required|max:255',
         ]);
+        
         // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
-        // タスクを更新
-        $task->status = $request->status;    // 追加
-        $task->content = $request->content;
-        $task->save();
-
+        
+        // 認証済みユーザ（閲覧者）の投稿として更新（リクエストされた値をもとに更新）
+        $request->user()->tasks()->create([
+            'content' => $request->content,
+            'status' => $request->status,
+        ]);
+        
         // トップページへリダイレクトさせる
         return redirect('/');
     }
@@ -146,7 +148,7 @@ class TasksController extends Controller
     public function destroy($id)
     {
         // idの値で投稿を検索して取得
-        $task = \App\Task::findOrFail($id);
+        $task = Task::findOrFail($id);
 
         // 認証済みユーザ（閲覧者）がその投稿の所有者である場合は、投稿を削除
         if (\Auth::id() === $task->user_id) {
--- database/migrations/2021_07_04_123751_add_user_id_to_messages_table.php
+++ database/migrations/2021_07_04_123751_add_user_id_to_messages_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddUserIdToTasksTable extends Migration
+class AddUserIdToMessagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,12 +14,9 @@ class AddUserIdToTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-           $table->unsignedBigInteger('user_id');
-           // 外部キー制約
-            $table->foreign('user_id')->references('id')->on('users');
+           $table->unsignedBigInteger('user_id');    
         });
-    }   
-        
+    }
 
     /**
      * Reverse the migrations.
@@ -29,7 +26,6 @@ class AddUserIdToTasksTable extends Migration
     public function down()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->dropIfExists('tasks');
             $table->dropForeign('tasks_user_id_foreign');
         });
     }
